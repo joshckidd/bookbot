bdb12226a42bc32c3d83b19d233daa0780edb54a
joshckidd <josh@larameekidd.com>
2025-07-10 08:43:05 -0400

    added character count

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, count_characters
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -6,8 +6,11 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
-    num_words = count_words(get_book_text("books/frankenstein.txt"))
+    book_text = get_book_text("books/frankenstein.txt")
+    num_words = count_words(book_text)
+    char_counts = count_characters(book_text)
     print(f"{num_words} words found in the document")
+    print(char_counts)
 
 main()
 
--- stats.py
+++ stats.py
@@ -1,2 +1,11 @@
 def count_words(book_text):
     return len(book_text.split())
+
+def count_characters(book_text):
+    char_counts = {}
+    for char in book_text.lower():
+        if char in char_counts:
+            char_counts[char] += 1
+        else:
+            char_counts[char] = 1
+    return char_counts
